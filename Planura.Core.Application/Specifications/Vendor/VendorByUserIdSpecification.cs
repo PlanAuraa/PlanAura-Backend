0b7d5084b2a9334d1c2530d940daa0f667fcb031
3moJunioR <m.wa7ba@gmail.com>
2026-07-13 01:08:01 +0300

    update fl auth

--- Planura.Core.Application/Specifications/Vendor/VendorByUserIdSpecification.cs
+++ Planura.Core.Application/Specifications/Vendor/VendorByUserIdSpecification.cs
@@ -3,7 +3,7 @@ using Planura.Core.Domain.Repositories;
 
 namespace Planura.Core.Application.Specifications;
 
-public class VendorByUserIdSpecification : BaseSpecification<Vendor>
+public class VendorByUserIdSpecification : BaseSpecification<Planura.Core.Domain.Entities.Vendor>
 {
     public VendorByUserIdSpecification(long userId)
         : base(vendor => vendor.UserId == userId)
