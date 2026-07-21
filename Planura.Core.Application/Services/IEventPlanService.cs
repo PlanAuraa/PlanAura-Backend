abca199b6783a32003a24f37720274b0cbb20629
Ibrahim Mohamed <hemaabokhalil677@gmail.com>
2026-07-21 19:33:49 +0300

    Add Update Event

--- Planura.Core.Application/Services/IEventPlanService.cs
+++ Planura.Core.Application/Services/IEventPlanService.cs
@@ -7,5 +7,6 @@ public interface IEventPlanService
     Task<EventPlanDto> CreateEventPlanAsync(long clientUserId, CreateEventPlanDto dto);
     Task<IEnumerable<EventPlanDto>> ListMyEventPlansAsync(long clientUserId);
     Task<EventPlanDto> GetEventPlanAsync(long eventPlanId, long clientUserId);
+    Task<EventPlanDto> UpdateEventPlanAsync(long eventPlanId, long clientUserId, UpdateEventPlanDto dto);
     Task DeleteEventPlanAsync(long eventPlanId, long clientUserId);
 }
